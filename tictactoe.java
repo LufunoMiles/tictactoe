2a2e56b8725698dc339f71676dbd4edf1f9f725d
Mukwevho Lufuno <99746961+LufunoMiles@users.noreply.github.com>
2025-04-14 22:02:29 +0200

    added action listeners

--- tictactoe.java
+++ tictactoe.java
@@ -1,7 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class  Fdemo extends Frame implements ActionListener{
+    Fdemo(){
+    setLayout(null);
+    setVisible(true);
+    setSize(800,600);
+    setLocation(400,100);
+    setBackground(Color.red);
+    setForeground(Color.white);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        
+    }
+
+}
+
 public class tictactoe {
  
 public static void main(String[] args) {
-    System.out.println("Welcome to Tic Tac Toe!");
+    
 }
 
 }
